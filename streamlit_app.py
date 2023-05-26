3376ad455535efe8723462898b778dee9ceea96b
jeandasim <134741306+jeandasim@users.noreply.github.com>
2023-05-26 19:53:31 -0300

    Calculadora_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ def sum_numbers(num1, num2):
     return num1 + num2
 
 def main():
-    st.title("Calculadora de Soma")
+    st.title("AISIM ENGENHARIA : Calculadora de Soma: ")
     
     num1 = st.number_input("Digite o primeiro número:")
     num2 = st.number_input("Digite o segundo número:")
@@ -17,6 +17,9 @@ def main():
     result = sum_numbers(num1, num2)
     
     st.write(f"A soma de {num1} e {num2} é igual a {result}")
+     
+st.write(" Esse é o primeiro exemplo de um aplicativo web elaborado em Python e compartilhado utilizando o Streamlit.")
+st.write(" Desenvolvido por Jean Baptiste Joseph em 26-05-2023 com auxílio do Chat-GPT.")
 
 if __name__ == "__main__":
     main()
